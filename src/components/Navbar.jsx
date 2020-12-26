0ee251a2160ced3af8d03a712eb55c43c5ef2cea
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 01:34:22 +0100

    navbar of app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { BsFillChatFill } from 'react-icons/bs'
+import { FaFootballBall } from 'react-icons/fa'
+import { IoMdHelpCircle } from 'react-icons/io'
+
+const Navbar = () => {
+    const itemStyle = {
+        borderTop: 'solid 1px #b2bec3',
+        color: '#000000'
+    }
+
+    const activeItemStyle = {
+        borderTop: "solid 2px #218c74", 
+        color: "#218c74"
+    }
+
+    return (
+        <nav className="fixed bottom-0 left-0 right-0 h-12 nav">
+            <div className="flex flex-row flex-nowrap w-full h-full text-2xl">
+                <NavLink exact to="/chat" style={itemStyle} className="w-full h-full focus:outline-none" activeStyle={activeItemStyle}><BsFillChatFill className="mx-auto my-3"/></NavLink>
+                <NavLink exact to="/" style={itemStyle} className="w-full h-full focus:outline-none" activeStyle={activeItemStyle}><FaFootballBall className="mx-auto my-3"/></NavLink>
+                <NavLink exact to="/help" style={itemStyle} className="w-full h-full focus:outline-none" activeStyle={activeItemStyle}><IoMdHelpCircle className="mx-auto my-3"/></NavLink>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
+
+
