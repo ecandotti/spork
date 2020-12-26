06e89e50b2036a0108b504efd2d1aaffb7f4d541
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 01:35:07 +0100

    Home section

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Search from '../components/Search'
+
+
+const Home = () => {
+    return (
+        <div className="px-12 py-16">
+            <Search />
+        </div>
+    )
+}
+
+export default Home
