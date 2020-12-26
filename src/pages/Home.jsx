3e43fbe990dc843e3766be85adb9026c83b799be
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 19:52:47 +0100

    wait....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import Search from '../components/Search'
+import { HomeContext } from '../contexts/useHome'
 
+import Search from '../components/home/Search'
+import MatchItem from '../components/home/MatchItem'
 
 const Home = () => {
+    const [allMatch, setAllMatch] = useState([])
+
+    const fetchMatch = async () => {
+        const API_URL = `http://api`
+        
+        await axios.get(API_URL)
+            .then(res => {
+                console.log(res)
+                console.log('Data updated')
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    useEffect(() => {
+        console.log('Updating data')
+        fetchMatch()
+    }, [allMatch])
+
     return (
-        <div className="px-12 py-16">
-            <Search />
-        </div>
+        <HomeContext.Provider value={{allMatch, setAllMatch}}>
+            <div className="px-12 py-16">
+                <Search />
+                <div className="flex flex-col flex-nowrap w-full">
+                    { allMatch.map((match) => <MatchItem match={match}/>) }
+                </div>
+            </div>
+        </HomeContext.Provider>
     )
 }
 
