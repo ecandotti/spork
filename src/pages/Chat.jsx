2333d68521a9d5f8faf395966bbb0d2f1f69087c
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 01:35:01 +0100

    chat section

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Chat = () => {
+    return (
+        <div className="px-12 pt-16">
+            <h1>Salut de CHAT</h1>
+        </div>
+    )
+}
+
+export default Chat
