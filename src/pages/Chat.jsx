a8db0ef493d7e4ad1b658a7cb045705f797e5d29
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 08:45:31 +0100

    implement socket.io

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+
+import useChat from '../components/chats/useChat'
+
+import WhichSport from '../components/chats/WhichSport'
 
 
 const Chat = () => {
+    const [roomId, setRoomId] = useState(0)
+    const { messages, sendMessage } = useChat(roomId) // Creates a websocket and manages messaging
+    const [newMessage, setNewMessage] = useState("")
+
+    const handleSendMessage = () => {
+        sendMessage(newMessage)
+        setNewMessage("")
+    }
+
+    const showCurrentChat = () => {
+        switch (roomId) {
+            case 1:
+                return ("Football")
+                break
+            case 2:
+                return ("Tennis")
+                break
+            case 3:
+                return ("Volleyball")
+                break
+            default:
+                return ("")
+                break
+        }
+    }
+
     return (
-        <div className="px-12 pt-16">
-            <h1>Salut de CHAT</h1>
+        <div className="px-10 py-16 h-full w-full">
+            <div className="text-center">
+                <span className="font-bold sm:text-4xl text-2xl">Choose room ID</span>
+            </div>
+
+            <WhichSport setRoomId={setRoomId}/>
+            
+            
+            { 
+                roomId 
+                ?
+                <>
+                    <Redirect to={`/chat/${roomId}`}/>
+                    {showCurrentChat()}
+                    <div className="border rounded-md border-gray-200 bg-gray-50 p-2 h-1/2">
+                        <div className="">
+                            <ol className="">
+                                {messages.map((message, i) => (
+                                    <li key={i} className="">
+                                        {message.body}
+                                    </li>
+                                ))}
+                            </ol>
+                        </div>
+                    </div>
+                    <div className="flex flex-row flex-nowrap items-center w-full my-4">
+                        <input
+                            type="text"
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                            placeholder="Write your message..."
+                            className="rounded-xl p-2 focus:outline-none w-2/3"
+                            />
+                        <button onClick={handleSendMessage} className="border bg-blue-400 text-white rounded-xl ml-4 p-2 w-1/3">Send</button>
+                    </div>
+                </> 
+                : <Redirect to={`/chat`}/>
+            }
         </div>
     )
 }
