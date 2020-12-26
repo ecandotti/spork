fee94cd566223e9169057657be1945e5ba0f91c3
ecandotti <enzo.candotti16@hotmail.com>
2020-12-26 19:51:41 +0100

    add routes

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Chat from '../pages/Chat'
 import Help from '../pages/Help'
+import AddTemp from '../pages/AddTemp'
 
 import Error from '../components/Error'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Load from '../components/Load'
+import MatchItem from '../components/home/MatchItem'
+import TeamItem from '../components/home/TeamItem'
 
 
 const Default = () => {
@@ -21,6 +24,9 @@ const Default = () => {
                 <Header />
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/admin/add" component={AddTemp} />
+                    <Route exact path="/match/:matchID" component={MatchItem} />
+                    <Route exact path="/team/:teamID" component={TeamItem} />
                     <Route exact path="/chat" component={Chat} />
                     <Route exact path="/chat/:roomID" component={Chat} />
                     <Route exact path="/help" component={Help} />
