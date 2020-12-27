4e7a8c228ee86b96884864120b79bc940c033ba8
ecandotti <enzo.candotti16@hotmail.com>
2020-12-27 06:48:47 +0100

    move add button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { BsFillChatFill } from 'react-icons/bs'
 import { FaFootballBall } from 'react-icons/fa'
-import { IoMdHelpCircle, IoMdAddCircleOutline } from 'react-icons/io'
+import { IoMdHelpCircle } from 'react-icons/io'
 
 const Navbar = () => {
     const itemStyle = {
@@ -22,7 +22,6 @@ const Navbar = () => {
                 <NavLink exact to="/" style={itemStyle} className="w-full h-full focus:outline-none" activeStyle={activeItemStyle}><FaFootballBall className="mx-auto my-3"/></NavLink>
                 <NavLink exact to="/help" style={itemStyle} className="w-full h-full focus:outline-none" activeStyle={activeItemStyle}><IoMdHelpCircle className="mx-auto my-3"/></NavLink>
             </div>
-                <NavLink exact to="/admin/add" className="absolute top-2 right-2 border bg-pink-400 p-2 rounded-md focus:outline-none"><IoMdAddCircleOutline /></NavLink>
         </nav>
     )
 }
