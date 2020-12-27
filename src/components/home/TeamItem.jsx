ad98adbcb250019cfccfad4f3ba44f62474ba480
ecandotti <enzo.candotti16@hotmail.com>
2020-12-27 06:50:59 +0100

    update component

--- src/components/home/TeamItem.jsx
+++ src/components/home/TeamItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const TeamItem = () => {
+const TeamItem = (props) => {
     return (
-        <div>
-            ok
+        <div className="flex flex-row flex-nowrap items-center border border-gray-100 bg-white rounded-lg p-4 my-2 cursor-pointer shadow-sm">
+            <img src={props.team.url_img} width="20%" height="auto" className="rounded-xl"/>
+            <span className="font-bold mx-auto">{props.team.name}</span>
         </div>
     )
 }
